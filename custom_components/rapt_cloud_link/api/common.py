503ccdd0da6e3e711533b43ca4080db8905f4c81
Martin <martinbirgell@gmail.com>
2025-07-24 11:42:04 +0200

    fix wrong url for token

--- custom_components/rapt_cloud_link/api/common.py
+++ custom_components/rapt_cloud_link/api/common.py
@@ -1,4 +1,4 @@
-from ..const import DEFAULT_API_BASE, DEV_API_BASE
+from ..const import DEFAULT_API_BASE, DEV_API_BASE, DEFAULT_TOKEN_URL, DEV_TOKEN_URL
 
 def get_api_base(entry):
     """Return the correct API base depending on config entry options."""
@@ -8,4 +8,4 @@ def get_api_base(entry):
 def get_token_url(entry):
     """Return the correct token URL depending on config entry options."""
     use_dev = entry.options.get("use_dev_api", False)
-    return "https://dev.rapt.io/api/token" if use_dev else "https://api.rapt.io/api/token"
+    return DEV_TOKEN_URL if use_dev else DEFAULT_TOKEN_URL
